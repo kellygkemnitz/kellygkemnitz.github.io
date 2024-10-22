eec32825f654644f2e7e04dde227fca9245d7e7c
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-10-22 14:57:08 -0500

    Adding new routes

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, send_from_directory
 import subprocess
 
 app = Flask(__name__)
@@ -7,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('flask_app/index.html')
+
+@app.route('/robots.txt')
+def robots_txt():
+    return send_from_directory(app.static_folder, 'robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap_xml():
+    return send_from_directory(app.static_folder, 'sitemap.xml'
 
 if __name__ == "__main__":
     app.run(debug=True)
